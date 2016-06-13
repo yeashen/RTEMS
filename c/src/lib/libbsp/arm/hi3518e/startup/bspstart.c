92fd13996f274f0e523feb14b0e02fefe7141958
xiaoming <xiaoming@sharpnow.com>
2016-06-13 10:59:00 +0800

    remove bspstart c code

--- c/src/lib/libbsp/arm/hi3518e/startup/bspstart.c
+++ c/src/lib/libbsp/arm/hi3518e/startup/bspstart.c
@@ -20,30 +20,10 @@
 #include <hi3518e.h>
 
 /*
- *  Show system relation information
- */
-static void bsp_show_info(void)
-{
-	uint32_t i, sysid[4], version = 0;
-	
-	for(i = 0; i < 4; i++){
-		sysid[i] = HI_REG_RD(SYSID_RGE_BASE+i*4);
-		version += sysid[i] << (i*8);
-	}
-	
-	printk("\n#################################################\n");
-	printk("@	RTEMS Real Time Operating System\n");
-	printk("@	System version: V4.11\n");
-	printk("@	Cpu version: Hi%x\n", version);
-	printk("#################################################\n\n");
-}
-
-/*
  *  BSP Specific Initialization in C
  */
 void bsp_start(void)
 {
-	bsp_show_info();
 	rtems_exception_init_mngt();
 	bsp_interrupt_initialize();
 }
